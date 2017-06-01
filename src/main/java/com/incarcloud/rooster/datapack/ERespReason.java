53a22df21583f6621a85e5260bb4208e6c172850
XGH <ghxiong@incarcloud.com>
2017-06-01 10:49:34 +0800

    createResponse & destroyResponse

---
+++ src/main/java/com/incarcloud/rooster/datapack/ERespReason.java
@@ -0,0 +1,11 @@
+package com.incarcloud.rooster.datapack;
+
+/**
+ * 应答原因
+ */
+public enum ERespReason {
+    NA,
+    OK,
+    Failed,
+    Heartbeat
+}
--- src/main/java/com/incarcloud/rooster/datapack/IDataParser.java
+++ src/main/java/com/incarcloud/rooster/datapack/IDataParser.java
@@ -5,8 +5,12 @@ import io.netty.buffer.ByteBuf;
 import java.util.List;
 
 public interface IDataParser {
-     // 抽取出完整有效的数据包,并从buffer丢弃掉已经解析或无用的字节
-     List<DataPack> extract(ByteBuf buffer);
+    // 抽取出完整有效的数据包,并从buffer丢弃掉已经解析或无用的字节
+    List<DataPack> extract(ByteBuf buffer);
+    // 创建应答数据包
+    ByteBuf createResponse(DataPack requestPack, ERespReason reason);
+    // 销毁应答数据包
+    void destroyResponse(ByteBuf responseBuf);
 }
 
 
