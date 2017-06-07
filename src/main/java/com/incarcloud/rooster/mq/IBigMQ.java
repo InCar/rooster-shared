a3a4cd1462fedbc0147724f311292c5a8d1661c6
youbeiwuhuan <bbfan@incarcloud.com>
2017-06-07 17:05:00 +0800

    增加单个发送方法

--- src/main/java/com/incarcloud/rooster/mq/IBigMQ.java
+++ src/main/java/com/incarcloud/rooster/mq/IBigMQ.java
@@ -5,10 +5,25 @@ import java.util.List;
 
 /** 
 * @ClassName: IBigMQ 
-* @Description: (这里用一句话描述这个类的作用) 
+* @Description: 操作消息队列的接口
 * @author Xiong Guanghua
 * @date 2017年6月2日 下午3:55:17 
 */
 public interface IBigMQ {
+
+
+	/**
+	 * 发送单个消息
+	 *
+	 * @param msg 消息
+	 * @return 发送结果
+	 */
+	MqSendResult post(MQMsg msg);
+
+	/**
+	 * 批量发送消息
+	 * @param listMsgs 消息列表
+	 * @return 发送结果
+	 */
 	List<MqSendResult> post(List<MQMsg> listMsgs);
 }
