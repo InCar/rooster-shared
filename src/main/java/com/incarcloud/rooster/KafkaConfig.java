fff2107a50d8dd40409ebc338f4e9c731019152a
youbeiwuhuan <fan794919764@126.com>
2017-06-30 15:29:33 +0800

    kafka 配置

---
+++ src/main/java/com/incarcloud/rooster/KafkaConfig.java
@@ -0,0 +1,11 @@
+package com.incarcloud.rooster;/**
+ * Created by fanbeibei on 2017/6/30.
+ */
+
+/**
+ * @author Fan Beibei
+ * @Description: 描述
+ * @date 2017/6/30 15:15
+ */
+public class KafkaConfig {
+}
