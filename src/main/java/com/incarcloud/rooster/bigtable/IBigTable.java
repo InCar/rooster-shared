cbc92075c919a1962161f462b61470f56b75e186
youbeiwuhuan <fan794919764@126.com>
2017-07-05 11:35:43 +0800

    保存接口

---
+++ src/main/java/com/incarcloud/rooster/bigtable/IBigTable.java
@@ -0,0 +1,11 @@
+package com.incarcloud.rooster.bigtable;/**
+ * Created by fanbeibei on 2017/7/5.
+ */
+
+/**
+ * @author Fan Beibei
+ * @Description: 描述
+ * @date 2017/7/5 9:43
+ */
+public interface IBigTable {
+}
