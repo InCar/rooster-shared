eaf9aac43177ffb9f72bfe6f7ad4fcaf2843d972
youbeiwuhuan <fan794919764@126.com>
2017-07-05 15:30:23 +0800

    工具类

---
+++ src/main/java/com/incarcloud/rooster/util/DataTargetUtils.java
@@ -0,0 +1,11 @@
+package com.incarcloud.rooster.util;/**
+ * Created by fanbeibei on 2017/7/5.
+ */
+
+/**
+ * @author Fan Beibei
+ * @Description: 描述
+ * @date 2017/7/5 14:50
+ */
+public class DataTargetUtils {
+}
