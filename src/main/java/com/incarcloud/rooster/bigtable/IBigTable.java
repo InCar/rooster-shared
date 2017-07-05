34a565608f2266fa52636ca0d6667acc2c946a92
youbeiwuhuan <fan794919764@126.com>
2017-07-05 11:43:23 +0800

    保存方法

--- src/main/java/com/incarcloud/rooster/bigtable/IBigTable.java
+++ src/main/java/com/incarcloud/rooster/bigtable/IBigTable.java
@@ -2,10 +2,24 @@ package com.incarcloud.rooster.bigtable;/**
  * Created by fanbeibei on 2017/7/5.
  */
 
+import com.incarcloud.rooster.datatarget.DataTarget;
+
 /**
  * @author Fan Beibei
- * @Description: 描述
+ * @Description: bigtable 操作抽象接口
  * @date 2017/7/5 9:43
  */
 public interface IBigTable {
+
+    /**
+     * 保存数据
+     * @param rowKey 行健
+     * @param data 数据
+     * @param tableName 表格名
+     * @throws Exception
+     */
+    public void save(String rowKey, DataTarget data, String tableName) throws Exception;
+
+
+
 }
