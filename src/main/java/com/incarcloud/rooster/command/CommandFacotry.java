bea0c0bd3b2dd5dd713650844adeb695516c50f4
youbeiwuhuan <fan794919764@126.com>
2017-07-07 17:49:37 +0800

    创建命令工厂类

---
+++ src/main/java/com/incarcloud/rooster/command/CommandFacotry.java
@@ -0,0 +1,11 @@
+package com.incarcloud.rooster.command;/**
+ * Created by fanbeibei on 2017/7/7.
+ */
+
+/**
+ * @author Fan Beibei
+ * @Description: 描述
+ * @date 2017/7/7 17:45
+ */
+public interface CommandFacotry {
+}
