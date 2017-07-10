f2f96a3bd530a7b5d4417d411d3a49362f447fd1
youbeiwuhuan <fan794919764@126.com>
2017-07-10 16:10:29 +0800

    工厂类

--- src/main/java/com/incarcloud/rooster/command/CommandFacotry.java
+++ src/main/java/com/incarcloud/rooster/command/CommandFacotry.java
@@ -4,8 +4,18 @@ package com.incarcloud.rooster.command;/**
 
 /**
  * @author Fan Beibei
- * @Description: 描述
+ * @Description: 工厂类，用于创建下发给车辆的命令
  * @date 2017/7/7 17:45
  */
 public interface CommandFacotry {
+
+    /**
+     * 创建二进制命令
+     *
+     * @param type     命令类型
+     * @param protocol 协议
+     * @return
+     */
+    byte[] createCommand(CommandType type, String protocol);
+
 }
