f4b911bda2264169808d25b0e0ceeb959eca3082
youbeiwuhuan <fan794919764@126.com>
2017-07-21 11:04:29 +0800

    调试通过

--- src/test/java/com/incarcloud/rooster/RestfulCommandClientTest.java
+++ src/test/java/com/incarcloud/rooster/RestfulCommandClientTest.java
@@ -3,6 +3,7 @@ package com.incarcloud.rooster;/**
  */
 
 import com.incarcloud.rooster.gather.cmd.CommandType;
+import com.incarcloud.rooster.gather.cmd.RespContent;
 import com.incarcloud.rooster.gather.cmd.client.CommandClient;
 import com.incarcloud.rooster.gather.cmd.client.RestfulCommandClient;
 import org.junit.Test;
@@ -17,7 +18,9 @@ public class RestfulCommandClientTest {
     public void testSend(){
         CommandClient client = new RestfulCommandClient("127.0.0.1:2181");
         try {
-            client.sendCommand("123456789", CommandType.FLASH_LIGHTS_ON);
+            RespContent resp =  client.sendCommand("123456789", CommandType.FLASH_LIGHTS_ON);
+
+            System.out.println(resp);
         }catch (Exception e){
             e.printStackTrace();
         }
