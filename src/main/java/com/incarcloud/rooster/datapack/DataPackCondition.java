b046fbb5e1ca6b82b37a4081e4e87ecca3ba0d91
youbeiwuhuan <fan794919764@126.com>
2017-08-29 14:50:56 +0800

    车身数据

---
+++ src/main/java/com/incarcloud/rooster/datapack/DataPackCondition.java
@@ -0,0 +1,11 @@
+package com.incarcloud.rooster.datapack;/**
+ * Created by fanbeibei on 2017/8/29.
+ */
+
+/**
+ * @author Fan Beibei
+ * @Description: 描述
+ * @date 2017/8/29 11:05
+ */
+public class DataPackCondition {
+}
