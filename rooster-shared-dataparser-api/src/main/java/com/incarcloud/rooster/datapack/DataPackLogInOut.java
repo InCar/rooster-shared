16619a14451f4cd82e33f9d9c88045fdedffb8e4
aaric <vipaaric@gmail.com>
2018-01-10 17:56:34 +0800

    2.1.14-SNAPSHOT

--- rooster-shared-dataparser-api/src/main/java/com/incarcloud/rooster/datapack/DataPackLogInOut.java
+++ rooster-shared-dataparser-api/src/main/java/com/incarcloud/rooster/datapack/DataPackLogInOut.java
@@ -1,7 +1,5 @@
 package com.incarcloud.rooster.datapack;
 
-import java.util.Arrays;
-
 /**
  * @author ChenZhao
  * @ClassName: DataPackLogInOut
@@ -10,111 +8,111 @@ import java.util.Arrays;
  */
 public class DataPackLogInOut extends DataPackObject {
 
-	/**
-	 * 登录类型：0-登录
-	 */
-	public static final int LOGIN_TYPE_LOGIN = 0;
-	/**
-	 * 登录类型：1-登出
-	 */
-	public static final int LOGIN_TYPE_LOGOUT = 1;
+    /**
+     * 登录类型：0-登录
+     */
+    public static final int LOGIN_TYPE_LOGIN = 0;
+    /**
+     * 登录类型：1-登出
+     */
+    public static final int LOGIN_TYPE_LOGOUT = 1;
 
-	/**
-	 * 登录类型 0 车辆登入 1 车辆登出
-	 */
-	private Integer loginType;
-	/**
-	 * 可充电储能子系统数
-	 */
-	private Integer sysNumber;
-	/**
-	 * 可充电储能系统编码长度
-	 */
-	private Integer codeLength;
-	/**
-	 * 可充电储能系统编码
-	 */
-	private String sysCode;
-	/**
-	 * 登入登出流水号
-	 */
-	private Integer serialNo;
+    /**
+     * 登录类型 0 车辆登入 1 车辆登出
+     */
+    private Integer loginType;
+    /**
+     * 可充电储能子系统数
+     */
+    private Integer sysNumber;
+    /**
+     * 可充电储能系统编码长度
+     */
+    private Integer codeLength;
+    /**
+     * 可充电储能系统编码
+     */
+    private String sysCode;
+    /**
+     * 登入登出流水号
+     */
+    private Integer serialNo;
 
-	/**
-	 * 加密密钥
-	 */
-	private Integer[] securityKey;
+    /**
+     * 加密密钥字符串(BASE64加密，使用转byte[])
+     */
+    private String securityKey;
 
-	public DataPackLogInOut(DataPackObject dataPack) {
-		super(dataPack);
-	}
+    public DataPackLogInOut(DataPackObject dataPack) {
+        super(dataPack);
+    }
 
-	/**
-	 * 软件版本号
-	 */
-	private String softwareVersion;
+    /**
+     * 软件版本号
+     */
+    private String softwareVersion;
 
-	public Integer[] getSecurityKey() {
-		return securityKey;
-	}
+    public String getSoftwareVersion() {
+        return softwareVersion;
+    }
 
-	public void setSecurityKey(Integer[] securityKey) {
-		this.securityKey = securityKey;
-	}
+    public void setSoftwareVersion(String softwareVersion) {
+        this.softwareVersion = softwareVersion;
+    }
 
-	public String getSoftwareVersion() {
-		return softwareVersion;
-	}
+    public Integer getLoginType() {
+        return loginType;
+    }
 
-	public void setSoftwareVersion(String softwareVersion) {
-		this.softwareVersion = softwareVersion;
-	}
+    public void setLoginType(Integer loginType) {
+        this.loginType = loginType;
+    }
 
-	public Integer getLoginType() {
-		return loginType;
-	}
+    public Integer getSysNumber() {
+        return sysNumber;
+    }
 
-	public void setLoginType(Integer loginType) {
-		this.loginType = loginType;
-	}
+    public void setSysNumber(Integer sysNumber) {
+        this.sysNumber = sysNumber;
+    }
 
-	public Integer getSysNumber() {
-		return sysNumber;
-	}
+    public Integer getCodeLength() {
+        return codeLength;
+    }
 
-	public void setSysNumber(Integer sysNumber) {
-		this.sysNumber = sysNumber;
-	}
+    public void setCodeLength(Integer codeLength) {
+        this.codeLength = codeLength;
+    }
 
-	public Integer getCodeLength() {
-		return codeLength;
-	}
+    public String getSysCode() {
+        return sysCode;
+    }
 
-	public void setCodeLength(Integer codeLength) {
-		this.codeLength = codeLength;
-	}
+    public void setSysCode(String sysCode) {
+        this.sysCode = sysCode;
+    }
 
-	public String getSysCode() {
-		return sysCode;
-	}
+    public Integer getSerialNo() {
+        return serialNo;
+    }
 
-	public void setSysCode(String sysCode) {
-		this.sysCode = sysCode;
-	}
+    public void setSerialNo(Integer serialNo) {
+        this.serialNo = serialNo;
+    }
 
-	public Integer getSerialNo() {
-		return serialNo;
-	}
+    public String getSecurityKey() {
+        return securityKey;
+    }
 
-	public void setSerialNo(Integer serialNo) {
-		this.serialNo = serialNo;
-	}
+    public void setSecurityKey(String securityKey) {
+        this.securityKey = securityKey;
+    }
 
-	@Override
-	public String toString() {
-		return "DataPackLogInOut [loginType=" + loginType + ", sysNumber=" + sysNumber + ", codeLength=" + codeLength
-				+ ", sysCode=" + sysCode + ", serialNo=" + serialNo + ", securityKey=" + Arrays.toString(securityKey)
-				+ ", softwareVersion=" + softwareVersion + "]";
-	}
+    @Override
+    public String toString() {
+        return "DataPackLogInOut [loginType=" + loginType + ", sysNumber=" + sysNumber + ", codeLength=" + codeLength
+                + ", sysCode=" + sysCode + ", serialNo=" + serialNo + ", securityKey=" + securityKey
+                + ", softwareVersion=" + softwareVersion + "]";
+    }
 
 }
