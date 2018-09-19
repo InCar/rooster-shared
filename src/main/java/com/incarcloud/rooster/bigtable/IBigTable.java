f0272129ff568fd7cf597dab3482c01053a647e1
aaric <vipaaric@gmail.com>
2018-09-19 19:23:53 +0800

    2.3.3-SNAPSHOT 优化IBigTable查询和RowKeyUtil工具类

--- src/main/java/com/incarcloud/rooster/bigtable/IBigTable.java
+++ src/main/java/com/incarcloud/rooster/bigtable/IBigTable.java
@@ -40,6 +40,16 @@ public interface IBigTable {
     <T extends DataPackObject> T getData(String rowKey, Class<T> clazz);
 
     /**
+     * 根据行键获得车辆数据
+     *
+     * @param rowKey 行键
+     * @param clazz  指定数据类型
+     * @param sort   默认按照时间倒序，排序规则：按照时间升序或者倒序
+     * @return
+     */
+    <T extends DataPackObject> T getData(String rowKey, Class<T> clazz, Sort sort);
+
+    /**
      * 根据车架号和时间段查询原始数据列表
      *
      * @param vin       车架号
@@ -62,7 +72,37 @@ public interface IBigTable {
     <T extends DataPackObject> List<T> queryData(String vin, Class<T> clazz, Integer pageSize, String startKey);
 
     /**
+     * 根据车架号、时间段分页查询数据列表
+     *
+     * @param vin       车架号
+     * @param clazz     指定数据类型
+     * @param sort      默认按照时间倒序，排序规则：按照时间升序或者倒序
+     * @param startTime 查询开始时间，如果有startKey，startTime设置无效
+     * @param endTime   查询结束时间
+     * @param pageSize  分页大小
+     * @param startKey  起始RowKey
+     * @return
+     */
+    <T extends DataPackObject> List<T> queryData(String vin, Class<T> clazz, Sort sort, Date startTime, Date endTime, Integer pageSize, String startKey);
+
+    /**
      * 关闭，回收资源
      */
     void close();
+
+    /**
+     * 排序规则
+     */
+    enum Sort {
+
+        /**
+         * 按照时间升序
+         */
+        ASC,
+
+        /**
+         * 按照时间倒序
+         */
+        DESC
+    }
 }
--- src/test/java/com/incarcloud/rooster/util/RowKeyUtilTest.java
+++ src/test/java/com/incarcloud/rooster/util/RowKeyUtilTest.java
@@ -13,14 +13,56 @@ public class RowKeyUtilTest {
 
     @Test
     public void testMakeRowKey() {
-        String rowKey = RowKeyUtil.makeRowKey("LSBAAAAAAZZ200000", "TRIP", "20180806113300");
+        String rowKey = RowKeyUtil.makeRowKey("LSBAAAAAAZZ000001", "TRIP", "20180910120000");
         System.out.println(rowKey);
-        Assert.assertEquals("221a000LSBAAAAAAZZ200000TRIP###########20180806113300####0001", rowKey);
+        Assert.assertEquals("bc3c000LSBAAAAAAZZ000001TRIP###########20180910120000####0001", rowKey);
+    }
+
+    @Test
+    public void testMakeMinRowKey1() {
+        String rowKey = RowKeyUtil.makeMinRowKey("LSBAAAAAAZZ000001");
+        System.out.println(rowKey);
+        Assert.assertEquals("bc3c000LSBAAAAAAZZ000001##", rowKey);
+    }
+
+    @Test
+    public void testMakeMinRowKey2() {
+        String rowKey = RowKeyUtil.makeMinRowKey("LSBAAAAAAZZ000001", "TRIP");
+        System.out.println(rowKey);
+        Assert.assertEquals("bc3c000LSBAAAAAAZZ000001TRIP#############", rowKey);
+    }
+
+    @Test
+    public void testMakeMinRowKey3() {
+        String rowKey = RowKeyUtil.makeMinRowKey("LSBAAAAAAZZ000001", "TRIP", "20180910120000");
+        System.out.println(rowKey);
+        Assert.assertEquals("bc3c000LSBAAAAAAZZ000001TRIP###########20180910120000######", rowKey);
+    }
+
+    @Test
+    public void testMakeMaxRowKey1() {
+        String rowKey = RowKeyUtil.makeMaxRowKey("LSBAAAAAAZZ000001");
+        System.out.println(rowKey);
+        Assert.assertEquals("bc3c000LSBAAAAAAZZ000001zz", rowKey);
+    }
+
+    @Test
+    public void testMakeMaxRowKey2() {
+        String rowKey = RowKeyUtil.makeMaxRowKey("LSBAAAAAAZZ000001", "TRIP");
+        System.out.println(rowKey);
+        Assert.assertEquals("bc3c000LSBAAAAAAZZ000001TRIP###########zz", rowKey);
+    }
+
+    @Test
+    public void testMakeMaxRowKey3() {
+        String rowKey = RowKeyUtil.makeMaxRowKey("LSBAAAAAAZZ000001", "TRIP", "20180910120000");
+        System.out.println(rowKey);
+        Assert.assertEquals("bc3c000LSBAAAAAAZZ000001TRIP###########20180910120000####zz", rowKey);
     }
 
     @Test
     public void testGetDataPackType() {
-        String rowKey = "221a000LSBAAAAAAZZ200000TRIP###########20180806113300####0001";
+        String rowKey = "bc3c000LSBAAAAAAZZ000001TRIP###########20180910120000####0001";
         String type = RowKeyUtil.getDataPackType(rowKey);
         System.out.println(type);
         Assert.assertEquals("TRIP", type);
