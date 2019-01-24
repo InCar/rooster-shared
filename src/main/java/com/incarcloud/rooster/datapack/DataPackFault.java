29be68305a29766c4b5f44abb2d1eec899845ce8
孔德亮 <kamil9660@163.com>
2019-01-24 14:33:48 +0800

    2.4.5-SNAPSHOT 修改故障、报警常量

--- src/main/java/com/incarcloud/rooster/datapack/DataPackFault.java
+++ src/main/java/com/incarcloud/rooster/datapack/DataPackFault.java
@@ -53,37 +53,37 @@ public class DataPackFault extends DataPackObject {
     public static final Map<Integer,Fault> alarmMap = new HashMap<Integer,Fault>(){
         {
             //电子制动力分配系统故障-EBD故障
-            put(0xF001,new DataPackFault.Fault(0,"COM_EBD","电子制动力分配系统")) ;
+            put(0xF001,new DataPackFault.Fault(0,"COM_FAULT_EBD","电子制动力分配系统")) ;
             //防抱死制动系统故障-ABS故障
-            put(0xF002,new DataPackFault.Fault(0,"COM_ABS","防抱死制动系统")) ;
+            put(0xF002,new DataPackFault.Fault(0,"COM_FAULT_ABS","防抱死制动系统")) ;
             //电子稳定系统故障-ESP故障
-            put(0xF003,new DataPackFault.Fault(0,"COM_ESP","车身电子稳定系统")) ;
+            put(0xF003,new DataPackFault.Fault(0,"COM_FAULT_ESP","车身电子稳定系统")) ;
             //自适应巡航系统故障-ACC故障
-            put(0xF004,new DataPackFault.Fault(0,"COM_ACC","自适应巡航控制系统")) ;
+            put(0xF004,new DataPackFault.Fault(0,"COM_FAULT_ACC","自适应巡航控制系统")) ;
             //前方防撞预警系统故障-FCM故障
-            put(0xF005,new DataPackFault.Fault(0,"COM_FCM","防撞预警系统")) ;
+            put(0xF005,new DataPackFault.Fault(0,"COM_FAULT_FCM","防撞预警系统")) ;
             //自动远光系统故障-AHB故障
-            put(0xF006,new DataPackFault.Fault(0,"COM_AHB","远光自动控制系统")) ;
+            put(0xF006,new DataPackFault.Fault(0,"COM_FAULT_AHB","远光自动控制系统")) ;
             //电动助力转向系统故障-EPS故障
-            put(0xF007,new DataPackFault.Fault(0,"COM_EPS","电子助力转向系统")) ;
+            put(0xF007,new DataPackFault.Fault(0,"COM_FAULT_EPS","电子助力转向系统")) ;
             //安全气囊故障-SRS故障
-            put(0xF008,new DataPackFault.Fault(0,"COM_SRS","汽车安全气囊系统")) ;
+            put(0xF008,new DataPackFault.Fault(0,"COM_FAULT_SRS","汽车安全气囊系统")) ;
             //发动机故障-GASOLINE故障
-            put(0xF009,new DataPackFault.Fault(0,"COM_GASOLINE","发动机系统")) ;
+            put(0xF009,new DataPackFault.Fault(0,"COM_FAULT_GASOLINE","发动机系统")) ;
             //变速箱故障-CVT故障
-            put(0xF00A,new DataPackFault.Fault(0,"COM_CVT","变速系统")) ;
+            put(0xF00A,new DataPackFault.Fault(0,"COM_FAULT_CVT","变速系统")) ;
             //胎压监测系统故障-TPMS故障
-            put(0xF00B,new DataPackFault.Fault(0,"COM_TPMS","轮胎压力监测系统")) ;
+            put(0xF00B,new DataPackFault.Fault(0,"COM_FAULT_TPMS","轮胎压力监测系统")) ;
             //重量感应系统故障-WSS故障
-            put(0xF00C,new DataPackFault.Fault(0,"COM_WSS","重量感应系统")) ;
+            put(0xF00C,new DataPackFault.Fault(0,"COM_FAULT_WSS","重量感应系统")) ;
             //弯道辅助照明系统故障-AFS故障
-            put(0xF00D,new DataPackFault.Fault(0,"COM_AFS","弯道辅助照明系统")) ;
+            put(0xF00D,new DataPackFault.Fault(0,"COM_FAULT_AFS","弯道辅助照明系统")) ;
             //制动辅助系统故障-BAS故障
-            put(0xF00E,new DataPackFault.Fault(0,"COM_BAS","制动辅助系统")) ;
+            put(0xF00E,new DataPackFault.Fault(0,"COM_FAULT_BAS","制动辅助系统")) ;
             //车辆出入管理系统故障-ETC故障
-            put(0xF00F,new DataPackFault.Fault(0,"COM_ETC","车辆出入管理系统")) ;
+            put(0xF00F,new DataPackFault.Fault(0,"COM_FAULT_ETC","车辆出入管理系统")) ;
             //主动防侧倾系统故障-ARS故障
-            put(0xF010,new DataPackFault.Fault(0,"COM_ARS","主动防侧倾系统")) ;
+            put(0xF010,new DataPackFault.Fault(0,"COM_FAULT_ARS","主动防侧倾系统")) ;
         }
     };
 
